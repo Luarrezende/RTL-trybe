fc75777629e7b1760614110946b66730edddb5cb
Luar <luarrezende@gmail.com>
2023-03-28 15:31:12 -0300

    resolve requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,82 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import App from '../App';
+
+describe('Testando About', () => {
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    // Acessar
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    const About = screen.getByRole('link', {
+      name: 'About',
+    });
+
+    // Agir
+    userEvent.click(About);
+
+    // Aferir
+    const title = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    // Acessar
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    const About = screen.getByRole('link', {
+      name: 'About',
+    });
+
+    // Agir
+
+    userEvent.click(About);
+
+    // Aferir
+    const paragraph1 = screen.getByText(
+      /this application simulates a pokédex, a digital encyclopedia containing all pokémon/i,
+    );
+    const paragraph2 = screen.getByText(
+      /one can filter pokémon by type, and see more details for each one of them/i,
+    );
+
+    expect(paragraph1).toBeInTheDocument();
+    expect(paragraph2).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    // Acessar
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    const About = screen.getByRole('link', {
+      name: 'About',
+    });
+
+    // Agir
+
+    userEvent.click(About);
+
+    // Aferir
+    const img = screen.getByRole('img', {
+      name: /pokédex/i,
+    });
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
-// import { act } from 'react-dom/test-utils';
 import App from '../App';
-// import renderWithRouter from '../renderWithRouter';
 
 describe('Testando App', () => {
   describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
@@ -33,101 +30,4 @@ describe('Testando App', () => {
       expect(Favorite).toBeInTheDocument();
     });
   });
-
-  // describe('Teste a rota /about', () => {
-  //   test('Navega para a rota /about pelo link', () => {
-  //     // Acessar
-  //     const { history } = renderWithRouter(<App />);
-  //     const aboutLink = screen.getByRole('link', {
-  //       name: 'Sobre',
-  //     });
-  //     expect(history.location.pathname).toBe('/');
-
-  //     // Agir
-  //     userEvent.click(aboutLink);
-
-  //     // Aferir
-  //     const title = screen.getByRole('heading', {
-  //       level: 1,
-  //       name: 'Sobre Mim',
-  //     });
-  //     expect(title).toBeInTheDocument();
-  //     expect(history.location.pathname).toBe('/about');
-  //   });
-  // });
-
-  // describe('Teste a rota /projects', () => {
-  //   test('Navega para a rota /projects pelo link', () => {
-  //     // Acessar
-  //     const { history } = renderWithRouter(<App />);
-  //     const projectsLink = screen.getByRole('link', {
-  //       name: /projetos/i,
-  //     });
-
-  //     // Agir
-  //     userEvent.click(projectsLink);
-
-  //     // Aferir
-  //     const title = screen.getByRole('heading', {
-  //       name: 'Meus Projetos',
-  //       level: 1,
-  //     });
-  //     expect(history.location.pathname).toBe('/projects');
-  //     expect(title).toBeInTheDocument();
-  //   });
-  // });
-
-  // describe('Teste uma rota inexistente', () => {
-  //   test('Mostra a página 404 quando navegado para uma rota inexistente', () => {
-  //     // Acessar
-  //     const { history } = renderWithRouter(<App />);
-
-  //     // Agir
-  //     // Fazer isso apenas para o history push
-  //     act(() => {
-  //       history.push('/xablau');
-  //     });
-
-  //     // Aferir
-  //     const rafaImage = screen.getByRole('img', {
-  //       name: /erro 404/i,
-  //     });
-  //     const title = screen.getByRole('heading', {
-  //       name: /404 page not found/i,
-  //     });
-  //     const subTitle = screen.getByRole('heading', {
-  //       name: /algo de errado não está certo/i,
-  //     });
-  //     expect(rafaImage).toBeInTheDocument();
-  //     expect(title).toBeInTheDocument();
-  //     expect(subTitle).toBeInTheDocument();
-  //   });
-  // });
-
-  // describe('Teste a rota /comments', () => {
-  //   test('Mostra se a página comments é renderizada ao clicar no link de comments', () => {
-  //     // Acessar
-  //     const { history } = renderWithRouter(<App />);
-
-  //     // Agir
-  //     const commentsLink = screen.getByRole('link', {
-  //       name: 'Comentários',
-  //     });
-
-  //     userEvent.click(commentsLink);
-  //     expect(history.location.pathname).toBe('/comments');
-
-  //     // Agir
-  //     const input = screen.getByRole('textbox');
-  //     const button = screen.getByRole('button', {
-  //       name: 'Deixe um comentário',
-  //     });
-  //     userEvent.type(input, 'Muito Bom! Parabéns');
-  //     userEvent.click(button);
-
-  //     // Aferir
-  //     const comment = screen.getByText('Muito Bom! Parabéns');
-  //     expect(comment).toBeInTheDocument();
-  //   });
-  // });
 });
