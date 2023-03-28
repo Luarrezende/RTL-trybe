110c35492648ccfe14941118b7db5490b9115e9a
Luar <luarrezende@gmail.com>
2023-03-28 16:35:51 -0300

    resolve requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import NotFound from '../pages/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    // Acessar
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <NotFound />
+      </Router>,
+    );
+    const subTitle = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+    const img = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+
+    // Aferir
+    expect(subTitle).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
